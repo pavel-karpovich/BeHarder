ea9acc56ad2e978def7b810897289a9c2a986e83
Pavel <Chesh397@mail.ru>
2018-06-03 15:59:28 +0000

    Test commit to see how work aws CD pipeline

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ var path = require('path');
 
 exports.get = function(event, context, callback) {
   var contents = fs.readFileSync(`public${path.sep}index.html`);
+  let a = 2;
   var result = {
     statusCode: 200,
     body: contents.toString(),
-    headers: {'content-type': 'text/html'}
+    headers: {
+      'content-type': 'text/html',
+      'content-lenght': '512'
+    }
   };
-
-  callback(null, result);
+  if (a + a === 4) {
+    callback(null, result);
+  }
 };
