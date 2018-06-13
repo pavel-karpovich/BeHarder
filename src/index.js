2e22ff871975d7da852e6d6b4370ad4808d64c0e
Pavel <Chesh397@mail.ru>
2018-06-13 17:21:50 +0300

    Update main svg, add transitions, open sans font

--- src/index.js
+++ src/index.js
@@ -1,25 +1,6 @@
 
 window.onload = (e) => {
-    /*
-    const items = [
-        document.getElementById('menu_item_1'),
-        document.getElementById('menu_item_2'),
-        document.getElementById('menu_item_3')
-    ];
-    
-    items.forEach(
-        item => {
-            item.addEventListener('focus',
-                e => e.target.parentElement.classList.add('menu-focus')
-                );
-            item.addEventListener('blur',
-                e => e.target.parentElement.classList.remove('menu-focus')
-                );
-            item.addEventListener('drag',
-                e => e.target.parentElement.classList.remove('menu-focus')
-                );
-            }
-    );*/
+
     document.getElementById('mobile_menu_btn')
         .addEventListener('click',
             e => {
@@ -30,4 +11,15 @@ window.onload = (e) => {
                 menu.classList.toggle('mobile-menu-toggle');
             }
         );
+    
+    [].forEach.call(document.getElementsByClassName('cls-3-doc'),
+        elm => elm.addEventListener('click',
+            e => {
+                console.log('1');
+                let sqr = e.currentTarget;
+                let check = document.getElementById('check_mark_' + sqr.id.slice(-1));
+                check.classList.toggle('invisible');
+            }
+        )
+    );
 };
