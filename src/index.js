4df678eb62f5c27364510b072cbb4b0b6163ad8c
Pavel <Chesh397@mail.ru>
2019-06-24 21:03:38 +0300

    Update dependencies

--- src/index.js
+++ src/index.js
@@ -2,26 +2,25 @@
 
 window.onload = () => {
 
-    document.getElementById('mobile_menu_btn')
-        .addEventListener('click',
-            e => {
-                let cls = e.currentTarget.firstElementChild.classList;
-                cls.toggle('icon-menu');
-                cls.toggle('icon-close');
-                let menu = document.getElementById('mobile_menu');
-                menu.classList.toggle('mobile-menu-toggle');
-            }
-        );
-    
-    [].forEach.call(document.getElementsByClassName('cls-5-doc'),
-        elm => elm.addEventListener('click',    
-            e => {
-                console.log('1');
-                let sqr = e.currentTarget;
-                let check = document.getElementById(
-                    'check_mark_' + sqr.id.slice(-1));
-                check.classList.toggle('invisible');
-            }
-        )
+  document.getElementById('mobile_menu_btn')
+    .addEventListener('click',
+      e => {
+        let cls = e.currentTarget.firstElementChild.classList;
+        cls.toggle('icon-menu');
+        cls.toggle('icon-close');
+        let menu = document.getElementById('mobile_menu');
+        menu.classList.toggle('mobile-menu-toggle');
+      }
     );
+
+  [].forEach.call(document.getElementsByClassName('cls-5-doc'),
+    elm => elm.addEventListener('click',
+      e => {
+        console.log('1');
+        let sqr = e.currentTarget;
+        let check = document.getElementById('check_mark_' + sqr.id.slice(-1));
+        check.classList.toggle('invisible');
+      }
+    )
+  );
 };
